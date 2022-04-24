aa90eed8ac9585c2e72933c749416039aad0258f
gijsn <61279464+gijsn@users.noreply.github.com>
2022-04-24 19:00:46 +0200

    add input from external wire Puts 80 on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,16 @@
 #define _TASK_SLEEP_ON_IDLE_RUN
 #include <TaskScheduler.h>
 
-
-#define PIN_LATCH D7 //ST_CP
-#define PIN_CLOCK D6 //SH_CP
-#define PIN_DATA D8 //DS
+#define PIN_LATCH D7 // ST_CP
+#define PIN_CLOCK D6 // SH_CP
+#define PIN_DATA D8  // DS
 
 #define PIN_LED1 D0
 #define PIN_LED2 D1
 #define PIN_LED3 D2
 #define PIN_LED4 D3
+
+#define PIN_INPUT D4
 
 Scheduler ts;
 
@@ -26,6 +27,7 @@ void led3Off();
 void led4On();
 void led4Off();
 void change7Segment();
+void inputCheck();
 
 // Tasks
 Task tLed1(100, TASK_FOREVER, &led1On, &ts, true);
@@ -33,25 +35,26 @@ Task tLed2(100, TASK_FOREVER, &led2On, &ts, true);
 Task tLed3(1000, TASK_FOREVER, &led3On, &ts, true);
 Task tLed4(500, TASK_FOREVER, &led4On, &ts, true);
 Task tsegment7(500, TASK_FOREVER, &change7Segment, &ts, true);
+Task tInput(100, TASK_FOREVER, &inputCheck, &ts, true);
 
 /*
  * setup() - this function runs once when you turn your Arduino on
  * We initialize the serial connection with the computer
  */
 
-
-void setup() 
+void setup()
 {
   // Set all the pins of 74HC595 as OUTPUT
   pinMode(PIN_LATCH, OUTPUT);
-  pinMode(PIN_DATA, OUTPUT);  
+  pinMode(PIN_DATA, OUTPUT);
   pinMode(PIN_CLOCK, OUTPUT);
+  pinMode(PIN_INPUT, INPUT_PULLUP);
 
   pinMode(PIN_LED1, OUTPUT);
   pinMode(PIN_LED2, OUTPUT);
   pinMode(PIN_LED3, OUTPUT);
   pinMode(PIN_LED4, OUTPUT);
-      digitalWrite(PIN_LED1, LOW);
+  digitalWrite(PIN_LED1, LOW);
   digitalWrite(PIN_LED2, LOW);
   digitalWrite(PIN_LED3, LOW);
   digitalWrite(PIN_LED4, LOW);
@@ -60,7 +63,8 @@ void setup()
   delay(50);
   digitalWrite(PIN_LED1, LOW);
   delay(100);
-  for(int i = 0; i < 100; i++){
+  for (int i = 0; i < 100; i++)
+  {
     digitalWrite(PIN_LED1, HIGH);
     delay(random(100));
     digitalWrite(PIN_LED1, LOW);
@@ -71,10 +75,10 @@ void setup()
 /*
  * loop() - this function runs over and over again
  */
-void loop() 
+void loop()
 {
 
-  	ts.execute();
+  ts.execute();
   // leds = 0;	// Initially turns all the LEDs off, by giving the variable 'leds' the value 0
   // updateShiftRegister();
   // delay(500);
@@ -86,7 +90,7 @@ void loop()
   // {
   //   bitSet(leds, i);		// Set the bit that controls that LED in the variable 'leds'
   //   updateShiftRegister();
-    
+
   //     digitalWrite(PIN_LED1, LOW);
   // digitalWrite(PIN_LED2, HIGH);
   // digitalWrite(PIN_LED3, LOW);
@@ -109,79 +113,101 @@ void loop()
  */
 void updateShiftRegister(uint16_t data)
 {
-
+}
+bool cableCon = false;
+void inputCheck()
+{
+  if (digitalRead(PIN_INPUT) == LOW)
+  {
+    cableCon = true;
+    digitalWrite(PIN_LATCH, LOW);
+    shiftOut(PIN_DATA, PIN_CLOCK, LSBFIRST, B00000000);
+    shiftOut(PIN_DATA, PIN_CLOCK, LSBFIRST, B00000000);
+    digitalWrite(PIN_LATCH, HIGH);
+  }
+  else
+  {
+    cableCon = false;
+  }
 }
 
-
-
-
-
-
-
-void led1On() {
+void led1On()
+{
   digitalWrite(PIN_LED1, HIGH);
   tLed1.setCallback(led1Off);
   // tLed1.setInterval(random(100));
   // tLed1.restart();
 }
 
-void led1Off(){
+void led1Off()
+{
   digitalWrite(PIN_LED1, LOW);
   tLed1.setCallback(led1On);
   // tLed1.setInterval(random(100));
   // tLed1.restart();
 }
 
-void led2On() {
+void led2On()
+{
   digitalWrite(PIN_LED2, HIGH);
   tLed2.setCallback(led2Off);
   tLed2.setInterval(random(200));
 }
 
-void led2Off(){
+void led2Off()
+{
   digitalWrite(PIN_LED2, LOW);
   tLed2.setCallback(led2On);
   tLed2.setInterval(random(2000));
 }
 
-void led3On() {
+void led3On()
+{
   digitalWrite(PIN_LED3, HIGH);
   tLed3.setCallback(led3Off);
   tLed3.setInterval(random(1000));
 }
 
-void led3Off(){
+void led3Off()
+{
   digitalWrite(PIN_LED3, LOW);
   tLed3.setCallback(led3On);
   tLed3.setInterval(random(1000));
 }
 
-void led4On() {
+void led4On()
+{
   digitalWrite(PIN_LED4, HIGH);
   tLed4.setCallback(led4Off);
   tLed4.setInterval(random(500));
 }
 
-void led4Off(){
+void led4Off()
+{
   digitalWrite(PIN_LED4, LOW);
   tLed4.setCallback(led4On);
   tLed4.setInterval(random(5000));
 }
 uint8_t disp1;
 uint8_t disp2;
-void change7Segment(){
-  bool chooseDisp = random(2);
-  if(chooseDisp){
-    disp1 = (uint8_t) random(256);
-  }else {
-    disp2 = (uint8_t) random(256);
+void change7Segment()
+{
+  if (cableCon)
+  {
+    return;
   }
-   digitalWrite(PIN_LATCH, LOW);
-   shiftOut(PIN_DATA, PIN_CLOCK, LSBFIRST, disp1);
-   shiftOut(PIN_DATA, PIN_CLOCK, LSBFIRST, disp2);
-   digitalWrite(PIN_LATCH, HIGH);
+  bool chooseDisp = random(2);
+  if (chooseDisp)
+  {
+    disp1 = (uint8_t)random(256);
+  }
+  else
+  {
+    disp2 = (uint8_t)random(256);
+  }
+  digitalWrite(PIN_LATCH, LOW);
+  shiftOut(PIN_DATA, PIN_CLOCK, LSBFIRST, disp1);
+  shiftOut(PIN_DATA, PIN_CLOCK, LSBFIRST, disp2);
+  digitalWrite(PIN_LATCH, HIGH);
   tsegment7.setInterval(random(200));
 }
-
-
-
